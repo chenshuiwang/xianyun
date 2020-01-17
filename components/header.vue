be41392eac464bb10bb34e3471afd79c3b5253f5
chen <3160495815@qq.com>
2020-01-17 16:49:39 +0800

    实现登录

--- components/header.vue
+++ components/header.vue
@@ -10,18 +10,41 @@
         <nuxt-link to="/hotel">酒店</nuxt-link>
         <nuxt-link to="/air">国内机票</nuxt-link>
       </el-row>
-      <div class="login">
+      <div class="login" v-if="!$store.state.user.userInfo.token">
         <nuxt-link to="/user/login">登录/注册</nuxt-link>
       </div>
+      <el-dropdown v-else>
+        <span class="el-dropdown-link">
+          <img :src="$axios.defaults.baseURL + $store.state.user.userInfo.user.defaultAvatar" />
+          <!-- <img :src="$axios.defaults.baseURL + $store.state.user.userInfo.user.defaultAvatar"> -->
+          <i class="el-icon-arrow-down el-icon--right"></i>
+        </span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>个人中心</el-dropdown-item>
+          <el-dropdown-item>退出</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
     </el-row>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  mounted() {
+    console.log(this.$store.state.user.name);
+  }
+};
 </script>
 
 <style lang='less' scoped>
+.el-dropdown-link {
+  img {
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
+    vertical-align: middle
+  }
+}
 .header-wrapper {
   height: 60px;
   background: #fff;
@@ -32,6 +55,9 @@ export default {};
   //z-index: 2;
 }
 .header {
+  width: 1000px;
+  height: 60px;
+  margin: 0 auto;
   .logo {
     img {
       width: 156px;
@@ -57,8 +83,8 @@ export default {};
     .nuxt-link-exact-active {
       background: #409eff;
       color: #fff;
-      &:hover{
-          color: #fff;
+      &:hover {
+        color: #fff;
       }
     }
   }
---
+++ store/user.js
@@ -0,0 +1,8 @@
+export const state = {
+    userInfo: {}
+}
+export const mutations = {
+    setUserInfo(state,data){
+        state.userInfo = data;
+    }
+}
